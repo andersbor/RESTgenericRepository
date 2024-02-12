dfa78a49b201141cb5bac9a08df732620ceba6e4
andersbor <anbo@zealand.dk>
2024-02-12 08:23:35 +0100

    Add project files.

---
+++ RESTgenericRepository/Book.cs
@@ -0,0 +1,11 @@
+﻿using GenericRepository;
+
+namespace RESTgenericRepository
+{
+    public class Book : IIdable
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ RESTgenericRepository/Controllers/BooksController.cs
@@ -0,0 +1,51 @@
+﻿using GenericRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RESTgenericRepository.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        private readonly Repository<Book> _repository;
+
+        public BooksController(Repository<Book> repository)
+        {
+            _repository = repository;
+        }
+
+        // GET: api/<BooksController>
+        [HttpGet]
+        public IEnumerable<Book> Get()
+        {
+            return _repository.GetAll();
+        }
+
+        // GET api/<BooksController>/5
+        [HttpGet("{id}")]
+        public Book? Get(int id)
+        {
+            return _repository.Get(id);
+        }
+
+        // POST api/<BooksController>
+        [HttpPost]
+        public Book Post([FromBody] Book value)
+        {
+            return _repository.Add(value);
+        }
+
+        // PUT api/<BooksController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<BooksController>/5
+        [HttpDelete("{id}")]
+        public Book? Delete(int id)
+        {
+            return _repository.Remove(id);
+        }
+    }
+}
